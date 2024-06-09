c9180423c23dbc1fbccd93057bedeeca7e8f6bb2
MadebyDaris <daris.idirene@gmail.com>
2024-06-09 12:53:37 +0200

    Updates

--- src/sphere.js
+++ src/sphere.js
@@ -1,5 +1,6 @@
 // Importation des librairies
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';export function sphereObj(scene, rayon) {
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+export function sphereObj(scene, rayon) {
     const material = new THREE.MeshPhongMaterial({
         color: new THREE.Color(171,171,171),
         transparent: true,
